1e036bccb7f86e540dd2e6db7339f02a3a8e84e2
Sanskar <sanskarjain2507@gmail.com>
2024-03-02 23:06:16 +0530

    settings

--- My_profile/settings.py
+++ My_profile/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*lom=s15f=0a$0aq*)rwdqff3(dqf9n0_xmuyzbvifb42f3-9v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1','e25b-49-207-201-61.ngrok-free.app']
 
 
 # Application definition
